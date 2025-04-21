8ec12d516dde72cb99811ee4956fa3e9a8afd501
João Guilherme <guioliveirarei@gmail.com>
2025-04-21 15:15:54 -0300

    JoaoG: Criação da animação de pulo faltando algumas alterações (3ºPessoa)

---
+++ tcc/Assets/Scripts/TempoDoPulo.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class TempoDoPulo : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Verifica se a animação está acontecendo e manda para o método TempoPulo() do Player.cs
+        animator.SendMessage("TempoPulo", true, SendMessageOptions.DontRequireReceiver);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Verifica se a animação acabou e manda para o método TempoPulo() do Player.cs
+        animator.SendMessage("TempoPulo", false, SendMessageOptions.DontRequireReceiver);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
